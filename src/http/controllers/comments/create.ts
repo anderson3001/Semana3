26cbaa527a98f042ca7754824e10724ef648f261
Anderson Gomes <derson3001@gmail.com>
2025-04-11 19:40:03 -0300

    feat: envio de email ao receber novo comentário

--- src/http/controllers/comments/create.ts
+++ src/http/controllers/comments/create.ts
@@ -1,11 +1,9 @@
 import { FastifyReply, FastifyRequest } from "fastify"
 import { z } from "zod"
-import { CreatePostUseCase } from "../../../use-cases/create-post-use-case"
-import { PrismaPostsRepository } from "../../../repositories/prisma/prisma-posts-repository"
-import { PrismaLikesRepository } from "../../../repositories/prisma/prisma-likes-repository"
-import { CreateLikeUseCase } from "../../../use-cases/create-like-use-case"
 import { PrismaCommentsRepository } from "../../../repositories/prisma/prisma-comments-repository"
 import { CreateCommentUseCase } from "../../../use-cases/create-comment-use-case"
+import { prisma } from "../../../lib/prisma"
+import nodemailer from "nodemailer"
 
 export async function create(request: FastifyRequest,reply: FastifyReply) {
     const createBodySchema = z.object({
@@ -26,6 +24,37 @@ export async function create(request: FastifyRequest,reply: FastifyReply) {
             userId,
             postId
         })
+
+        const post = await prisma.post.findUnique({
+            where: {
+                id: postId
+            },
+            include: { 
+                user: true
+            } 
+        })
+        if (!post || !post.user?.email) {
+            return reply.status(404).send({ message: "Autor do post não encontrado" })
+        }
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
+            }
+        })
+        let options = {
+            from: `"API do Anderson" <${process.env.EMAIL_USER}>`,
+            to: post.user.email,
+            subject: 'Novo comentário!',
+            text: `Olá, você recebeu um novo comentário no seu post!`,
+        }
+        try {
+            await transporter.sendMail(options);
+            console.log('Email enviado com sucesso!')
+        } catch (err) {
+            console.log('Erro ao enviar email:');
+        }
 
     } catch (err) {
         throw err
